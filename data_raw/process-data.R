fbf22aaa18a99dec33c80077598abe735379833e
Mauricio Vargas <mavargas11@uc.cl>
2022-07-24 21:30:12 -0400

    modified computed tables + data update

--- data_raw/process-data.R
+++ data_raw/process-data.R
@@ -9,6 +9,8 @@ library(usethis)
 fraw <- "data_raw/"
 try(dir.create(fraw))
 
+## treaties ----
+
 treaties_url <- "https://www.designoftradeagreements.org/media/filer_public/97/7f/977f7d18-2edb-4d94-ba30-16cf3ea2f824/desta_list_of_treaties_02_01.xlsx"
 treaties_xlsx <- paste0(fraw, gsub(".*/", "", treaties_url))
 treaties_rda <- "data/treaties.rda"
@@ -38,6 +40,8 @@ if (!file.exists(treaties_dyads_rda)) {
   load("data/treaties_dyads.rda")
 }
 
+## withdrawals ----
+
 withdrawals_dyads_url <- "https://www.designoftradeagreements.org/media/filer_public/77/6c/776c7757-3ae4-4dba-b2ad-b6d0f1f9c634/desta_dyadic_withdrawal_02_01.xlsx"
 withdrawals_dyads_xlsx <- paste0(fraw, gsub(".*/", "", withdrawals_dyads_url))
 withdrawals_dyads_rda <- "data/withdrawals_dyads.rda"
@@ -53,19 +57,48 @@ if (!file.exists(withdrawals_dyads_rda)) {
   load("data/withdrawals_dyads.rda")
 }
 
+## content coding ----
+
+content_coding_url <- "https://www.designoftradeagreements.org/media/filer_public/85/b2/85b2833f-ba14-42e7-8e8e-381dd4e354cd/desta_version_02_01.xlsx"
+content_coding_xlsx <- paste0(fraw, gsub(".*/", "", content_coding_url))
+content_coding_rda <- "data/content_coding.rda"
+
+if (!file.exists(content_coding_rda)) {
+  try(download.file(content_coding_url, content_coding_xlsx))
+  content_coding <- read_xlsx(content_coding_xlsx,
+                              sheet = "data") %>%
+    mutate_if(is.numeric, as.integer)
+  use_data(content_coding, overwrite = T)
+} else {
+  load("data/content_coding.rda")
+}
+
 # computed tables ----
 
-treaties <- treaties_dyads %>%
-  select(year, country1, country2, entry_type) %>%
+## current Free Trade Agreements ----
+
+treaties2 <- treaties_dyads %>%
+  select(number, year, country1, country2, entry_type) %>%
   filter(entry_type == "base_treaty") %>%
-  mutate(c1 = pmin(country1, country2), c2 = pmax(country1, country2)) %>%
+  mutate(c1 = pmin(country1, country2), c2 = pmax(country1, country2))
+
+# filter for full FTAs!!!
+
+treaties2 <- treaties2 %>%
+  inner_join(
+    content_coding %>%
+      select(number, mar_typedepth) %>%
+      filter(mar_typedepth == 2)
+  )
+
+treaties2 <- treaties2 %>%
   select(year, c1, c2) %>%
   distinct() %>%
   group_by(c1, c2) %>%
   summarise(year = min(year)) %>%
   mutate(rta = 1)
 
-withdrawals <- withdrawals_dyads %>%
+withdrawals2 <- withdrawals_dyads %>%
   select(year, country1, country2, entry_type) %>%
   mutate(c1 = pmin(country1, country2), c2 = pmax(country1, country2)) %>%
   ungroup() %>%
@@ -75,24 +108,24 @@ withdrawals <- withdrawals_dyads %>%
   summarise(year = min(year)) %>%
   mutate(rta = -1)
 
-valid_rta <- crossing(
-  year = min(treaties$year, withdrawals$year):max(treaties$year, withdrawals$year),
-  treaties %>% select(c1, c2)
+in_force_ftas_dyads <- crossing(
+  year = min(treaties2$year, withdrawals2$year):max(treaties2$year, withdrawals2$year),
+  treaties2 %>% select(c1, c2)
   ) %>%
   left_join(
-    treaties
+    treaties2
   ) %>%
   left_join(
-    withdrawals %>% rename(wrta = rta)
+    withdrawals2 %>% rename(wrta = rta)
   )
 
-valid_rta <- valid_rta %>%
+in_force_ftas_dyads <- in_force_ftas_dyads %>%
   group_by(c1, c2) %>%
   fill(rta, .direction = "down") %>%
   fill(wrta, .direction = "down") %>%
   ungroup()
 
-valid_rta <- valid_rta %>%
+in_force_ftas_dyads <- in_force_ftas_dyads %>%
   mutate(
     rta = if_else(is.na(rta), 0, rta),
     wrta = if_else(is.na(wrta), 0, wrta)
@@ -100,17 +133,17 @@ valid_rta <- valid_rta %>%
   group_by(c1,c2) %>%
   mutate(res = rta + wrta)
 
-valid_rta <- valid_rta %>%
+in_force_ftas_dyads <- in_force_ftas_dyads %>%
   ungroup() %>%
   select(-c(rta, wrta)) %>%
   rename(rta = res) %>%
   mutate_if(is.numeric, as.integer)
 
-current_treaties_dyads <- valid_rta %>%
+in_force_ftas_dyads <- in_force_ftas_dyads %>%
   rename(
     country1 = c1,
     country2 = c2,
-    at_least_one_valid_treaty = rta
+    in_force_fta = rta
   )
 
-use_data(current_treaties_dyads, overwrite = T)
+use_data(in_force_ftas_dyads, overwrite = T)
